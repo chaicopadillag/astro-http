87d0f2d4c48ff5619620d4012a16be09175d48f2
chaicopadillag <xt9738@rimac.com.pe>
2024-12-13 19:24:03 -0500

    add Cloudflare adapter and update dependencies in package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,19 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
+
+// import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  // adapter: node({
+  //   mode: 'standalone'
+  // })
+  integrations: [mdx(), sitemap()],
+
+  adapter: cloudflare()
 });
